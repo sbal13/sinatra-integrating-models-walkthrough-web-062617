acc2829138595cd13a7c3f1f29b1e28e43a4f248
sbal13 <sbal13@gmail.com>
2017-07-18 13:00:09 -0400

    Done.

--- app.rb
+++ app.rb
@@ -8,7 +8,8 @@ get '/' do
 end
 
 post '/' do
-  text_from_user = params[:user_text]
+  @text_from_user = params[:user_text]
+  @analyzed_text = TextAnalyzer.new(text_from_user)
   erb :results
 end
 
